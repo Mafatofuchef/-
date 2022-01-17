d26587676650baa6882b7cc97dbdd6fc7c0afc2d
박택민 <xorals723@gmail.com>
2022-01-17 14:47:14 +0900

    창이름변경시도1

--- main.py
+++ main.py
@@ -3,26 +3,33 @@ from cgitb import text
 from ctypes.wintypes import SIZE
 from curses import newwin
 import tkinter as tk
+import tkinter as insert
+import tkinter as party
 from turtle import width
+
+#입력창 설정
 
 def window():
     newWindow = tk.Toplevel(root)
+    newWindow.title("입력창")
     labelExample = tk.Label(newWindow, text="test2233",width=40, height=20)
     
     labelExample.pack()
 
 
+
+#편성창 설정
+
 def window2():
     newWindow = tk.Toplevel(root)
+    newWindow.title("편성창")
     labelExample = tk.Label(newWindow, text="test2213",width=40, height=20)
 
     labelExample.pack()
 
-root =tk.Tk()
-
-
 
 #메인에 대한 값들
+root =tk.Tk()
 
 root.title("파티 편집기 v.1")
 root.geometry("800x800")
@@ -34,12 +41,12 @@ Label_vs.place(x=400,y=400)
 #입력 버튼
 
 btn1=tk.Button(root, text="입력", command=window)
-btn1.pack()
+btn1.place(x=200,y=400)
 
 #편성버튼
 
 btn2=tk.Button(root, text="편성", command=window2)
-btn2.pack()
+btn2.place(x=600, y=400)
 
 root.mainloop()
 
